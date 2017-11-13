1844a6a9756ed94d5e145450b1053c8386589356
Rikard Fahlström <rikard.fahlstrom@gmail.com>
2017-11-13 06:25:59 +0100

    structure changes to files

--- project_area/webrepo/webrepo/views.py
+++ project_area/webrepo/webrepo/views.py
@@ -1,6 +1,11 @@
 from pyramid.view import view_config
-
+import webrepo.infrastructure.static_cache
 
 @view_config(route_name='home', renderer='templates/mytemplate.pt')
 def my_view(request):
     return {'project': 'My Web App'}
+
+
+def extend_model(model_dict):
+    model_dict['build_cache_id'] = webrepo.infrastructure.static_cache.build_cache_id()
+    return model_dict
