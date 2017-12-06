2fe531f4f65d88e5abb529c0fb70fc4019980aeb
Rikard Fahlström <rikard.fahlstrom@gmail.com>
2017-12-06 06:43:45 +0100

    implementad shared layout

--- project_area/webrepo/webrepo/controllers/base_controller.py
+++ project_area/webrepo/webrepo/controllers/base_controller.py
@@ -2,12 +2,21 @@
 
 import webrepo.infrastructure.static_cache as static_cache
 from webrepo.infrastructure.supressor import suppress
+import pyramid.renderers
 
 
 class BaseController:
     def __init__(self, request):
         self.request = request
         self.build_cache_id = static_cache.build_cache_id
+
+        layout_render = pyramid.renderers.get_renderer('webrepo:templates/shared/_layout.pt')
+        impl = layout_render.implementation()
+        self.layout = impl.macros['layout']
+
+    @property
+    def is_logged_in(self):
+        return True
 
     @suppress
     def dont_expose_as_web_action_base(self):
